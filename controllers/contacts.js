70ed506e3caa4a68779f1ae0630676cf8a8235d1
mckechniep@gmail.com <mckechniep@MagicalBox.>
2024-10-09 18:09:24 -0400

    delete and update added

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 
 //router logic
+//index route
 router.get("/", async (req, res) => {
   try {
     const user = await User.findById(req.session.user._id);
@@ -10,10 +11,11 @@ router.get("/", async (req, res) => {
     res.render("contacts/index.ejs", { contacts: user.contacts });
   } catch (error) {
     console.log(error);
-    res.redict("/");
+    res.redirect("/");
   }
 });
 
+//new route
 router.get("/new", (req, res) => {
   res.render("contacts/new.ejs");
 });
@@ -37,6 +39,7 @@ router.post("/", async (req, res) => {
   }
 });
 
+//create route ? post?
 router.get("/", async (req, res) => {
   try {
     // Look up the user from req.session
@@ -53,10 +56,49 @@ router.get("/", async (req, res) => {
   }
 });
 
+//show route
 router.get("/:contactId", async (req, res) => {
     const user = await User.findById(req.session.user._id);
     const contact = user.contacts.id(req.params.contactId);
   res.render("contacts/show.ejs", { contact });
 });
+
+// Edit route
+router.get("/:contactId/edit", async (req, res) => {
+    const user = await User.findById(req.session.user._id);
+    const contact = user.contacts.id(req.params.contactId);
+    res.render("contacts/edit.ejs", { contact });
+  });
+
+  
+// Update route
+router.put("/:contactId", async (req, res) => {
+    try {
+      const user = await User.findById(req.session.user._id);
+      const contact = user.contacts.id(req.params.contactId);
+      Object.assign(contact, req.body);
+      await user.save();
+      res.redirect(`/users/${user._id}/contacts/${contact._id}`);
+    } catch (error) {
+      console.log(error);
+      res.redirect("/");
+    }
+  });
+
+
+
+// Delete route
+router.delete("/:contactId", async (req, res) => {
+    try {
+      const user = await User.findById(req.session.user._id);
+      user.contact = user.contacts.filter(contact => contact._id.toString() !== req.params.contactId);
+      await user.save();
+      res.redirect(`/users/${user._id}/contacts`);
+    } catch (error) {
+      console.log(error);
+      res.redirect("/");
+    }
+  });
+
 
 module.exports = router;
