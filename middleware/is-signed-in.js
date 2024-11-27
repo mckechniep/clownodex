fff2a73cec0d3efac7d64ae8ff264ac3847e6221
Peter McKechnie <mckechniep@gmail.com>
2024-11-27 14:32:15 -0500

    comments added

--- middleware/is-signed-in.js
+++ middleware/is-signed-in.js
@@ -4,6 +4,12 @@ the session (provided by req.session.user).
 This is typically used to confirm that a user is logged in. */
 
 
+// Acts as a guard/gate for protected routes
+// Redirects to sign-in if no user is in session
+// Used to protect routes that require authentication
+// Blocks unauthorized access
+
+
 const isSignedIn = (req, res, next) => {
 if (req.session.user) return next();
 res.redirect('auth/sign-in');
--- middleware/pass-user-to-view.js
+++ middleware/pass-user-to-view.js
@@ -1,5 +1,11 @@
 //This middleware provides us a shortcut to always pass the information of the logged in user to our requests final destination.
 
+// Makes user data available to all views via res.locals
+// Doesn't block or redirect
+// Helps templates display user-specific content
+// Eliminates repetitive user data passing in routes
+
+
 const passUserToView = (req, res, next) => {
 res.locals.user = req.session.user ? req.session.user : null;
 next();
